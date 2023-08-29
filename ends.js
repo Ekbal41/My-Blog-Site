edf182c3aa5800ddc73f342c830e45ff4d597a32
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-08-29 14:30:30 +0600

    ok

--- ends.js
+++ ends.js
@@ -3,4 +3,7 @@ module.exports = napnux()
   // All routes for the project root goes here
   .get("/", (req, res) => {
     res.render("index");
+  })
+  .get("/blog", (req, res) => {
+    res.render("blog");
   });
