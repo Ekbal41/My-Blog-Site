7b8cf9a4a475095c008f2a7a96035e0cec52d49c
Asif Ekbal <103681582+Ekbal41@users.noreply.github.com>
2023-09-06 02:02:56 +0600

    Update ends.js

--- ends.js
+++ ends.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs').promises; // Use fs.promises for async operations
 const napnux = require("napnux");
 const { getFileMetaDatas } = require('./utils');
 const { about, projects, skills } = require("./data");
@@ -6,23 +6,30 @@ const path = require('path');
 const blogDir = path.join(process.cwd(), "data/blogs/");
 
 module.exports = napnux()
-  .get("/", (req, res) => {
-    const files = fs.readdirSync(blogDir);
-    const blogs = getFileMetaDatas(files);
-    const firstFiveBlogs = blogs.slice(0, 5);
-    res.render("index", { firstFiveBlogs, about, skills, projects });
+  .get("/", async (req, res) => {
+    try {
+      const files = await fs.readdir(blogDir);
+      const blogs = getFileMetaDatas(files);
+      const firstFiveBlogs = blogs.slice(0, 5);
+      res.render("index", { firstFiveBlogs, about, skills, projects });
+    } catch (error) {
+      // Handle errors
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
   })
-  .get("/blog/:slug", (req, res) => {
+  .get("/blog/:slug", async (req, res) => {
     const { slug } = req.params;
-    let rawfileContent = fs.readFileSync(blogDir + slug + ".md", 'utf8');
-    let fileContent = rawfileContent.replace(/---[\s\S]*?---/, '');
-    const files = fs.readdirSync(blogDir);
-    const blogs = getFileMetaDatas(files);
-    const blog = blogs.find(blog => blog.slug === slug);
-    res.render("blog/index", {
-      blog,
-      fileContent
-    });
+    try {
+      const rawfileContent = await fs.readFile(path.join(blogDir, `${slug}.md`), 'utf8');
+      const fileContent = rawfileContent.replace(/---[\s\S]*?---/, '');
+      const files = await fs.readdir(blogDir);
+      const blogs = getFileMetaDatas(files);
+      const blog = blogs.find(blog => blog.slug === slug);
+      res.render("blog/index", { blog, fileContent });
+    } catch (error) {
+      // Handle errors
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
   });
-
-
